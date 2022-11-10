744a8fec5510de3da95cd952066172788d8cb0cd
Kayla Friend <friendka@appstate.edu>
2022-11-10 15:18:59 -0500

    getting closer to html and content script connection

--- js/contentScript.js
+++ js/contentScript.js
@@ -1,4 +1,4 @@
-let b = document.body.style;
+b = document.body.style;
 
 //Change font
 b.fontFamily = 'opendyslexic';
@@ -6,4 +6,14 @@ b.fontStyle = 'normal';
 b.fontWeight = 'normal';
 
 //Change font color
-b.color = 'red';
+c = 'black';
+
+var fontC = document.getElementById("fontC");
+if (fontC) {
+    fontC.addEventListener("click", getOption);
+}
+
+function getOption() {
+    c = document.getElementById("fontC").selected;
+    b.color = c;
+}
--- js/popup.js
+++ js/popup.js
@@ -23,19 +23,5 @@ changeColor.addEventListener("click", async () => {
     });
   }
 
-  //Change Font Code
-  let changeFont = document.getElementById("changeFont");
-
-  changeFont.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      func: setPageFont,
-    });
-  });
-
-  function setPageFont() {
-    chrome.fontSettings.setFont(
-      { genericFamily: 'sansserif', script: 'Jpan', fontId: 'MS PGothic' }
-    );
-  }
+  //Change Font Color
+  
