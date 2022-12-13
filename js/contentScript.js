8259cbd32f7155b6d30fbd99550960b0e08277bc
Kayla Friend <friendka@appstate.edu>
2022-12-13 13:48:13 -0500

    Small adjustments

--- js/contentScript.js
+++ js/contentScript.js
@@ -1,47 +1,44 @@
 const b = document.body.style;
 
-//b.fontStyle = textF.style;
+// CHANGE FONT
 
-//Change font
-var textF = null;
-if (textF == null) {
-    textF = 'normal';
-}
-b.fontStyle = textF;
-
+// Set event listener for font style selecction
 var selectF = document.getElementById('font');
 if (selectF) {
     selectF.addEventListener("click", getOptionF);
 }
 
+// Get text selection and change font style
 function getOptionF() {
-    var textF = selectF.options[selectF.selectedIndex].text;
+    textF = selectF.options[selectF.selectedIndex].text;
     b.fontStyle = textF;
 }
 
-function getFont(f) {
-    b.fontStyle = f;
-}
-//Change font color
+// CHANGE FONT COLOR
+
+// Set event listener for font color selecction
 var select = document.getElementById('fontColor');
 if (select) {
     select.addEventListener("click", getOption);
 }
 
+// Get text selection and change font color
 function getOption() {
     text = select.options[select.selectedIndex].text;
     b.color = text;
 }
-//Change Background color
-//b.backgroundColor = 'purple';
+
+// CHANGE BACKGROUND COLOR
+
+// Set event listener for background color selecction
 var textBG = null;
 var selectBG = document.getElementById('bgColor');
 if (selectBG) {
     selectBG.addEventListener("click", getBG);
 }
 
+// Get text selection and change background color
 function getBG() {
     textBG = selectBG.options[selectBG.selectedIndex].text;
     b.backgroundColor = textBG;
 }
-b.backgroundColor = textBG;
